3b4a236ba336f6d8bcf86ff991769393dc6a37fe
Kim9xd <kim9xd@gmail.com>
2021-12-13 16:30:19 +0900

    * 값 타입   - 실전 예제 6 - 값 타입 매핑

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -11,10 +12,10 @@ public class Delivery extends BaseEntity {
 	
 	@Id @GeneratedValue
 	private Long id;
+
+	@Embedded
+	private Address address;
 	
-	private String city;
-	private String street;
-	private String zipcode;
 	private DeliveryStatus status;
 	
 	@OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
